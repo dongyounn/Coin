a4e8c365bdb68fbe4f85e90b157e256db65c806a
dongyoun_shin <dyshin1027@gmail.com>
2021-04-11 23:54:30 +0900

    [Dev] API 호출 기록 정리, 다음번에 AOP 적용해서 처리해보자

--- src/main/kotlin/com/coin/upbit/upbit/service/UpbitService.kt
+++ src/main/kotlin/com/coin/upbit/upbit/service/UpbitService.kt
@@ -1,14 +1,27 @@
 package com.coin.upbit.upbit.service
 
+import com.coin.upbit.global.exception.BadRequestException
+import com.coin.upbit.global.exception.ErrorReason
 import com.coin.upbit.global.message.upbit.CoinInfo
 import com.coin.upbit.global.message.upbit.UpbitApi
+import com.coin.upbit.upbit.domain.ResultEnum
+import com.coin.upbit.upbit.domain.UpbitApiCallHistory
+import com.coin.upbit.upbit.infra.repository.UpbitApiCallRepository
 import org.springframework.stereotype.Service
 
 @Service
 class UpbitService(
-        private val upbitApi: UpbitApi
+        private val upbitApi: UpbitApi,
+        private val upbitApiCallRepository: UpbitApiCallRepository
 ) {
     fun getCoinInfos(): CoinInfo {
-        return upbitApi.getCoinInfo()
+        val method = UpbitApiCallHistory.of("getCoinInfos", ResultEnum.FAIL)
+        try {
+            val result = upbitApi.getCoinInfo()
+            upbitApiCallRepository.save(method.setStatusSuccess())
+            return result
+        } catch (e: Exception) {
+            throw BadRequestException(ErrorReason.INVALID_DATA, "에러 발생")
+        }
     }
 }
